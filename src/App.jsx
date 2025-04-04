2151bedd7e5f1b7337a3fd6d1aa2dce181655b62
CodeMeNawh <89380204+CodeMeNawh@users.noreply.github.com>
2025-04-04 16:56:01 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import { CORE_CONCEPTS } from './data';
+import Header from './components/Header/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
+import { EXAMPLES } from './data';
+
+
+
+function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+  function handleClick(selectedButton){
+    
+    setSelectedTopic(selectedButton);
+  }
+  return (
+    <div>
+      <Header />
+
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+          {CORE_CONCEPTS.map((item)=>(<CoreConcept key={item.title} {...item}/>))}
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton isSelected={selectedTopic==="components"} onClick={()=>handleClick('components')}>Components</TabButton>
+            <TabButton isSelected={selectedTopic==="jsx"} onClick={()=>handleClick('jsx')}>JSX</TabButton>
+            <TabButton isSelected={selectedTopic==="props"} onClick={()=>handleClick('props')}>Props</TabButton>
+            <TabButton isSelected={selectedTopic==="state"} onClick={()=>handleClick('state')}>State</TabButton>
+          </menu>
+          {!selectedTopic ? <p>Please Select Topic</p> : null }
+          {selectedTopic ?  <div id="tab-content">
+            
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+              {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div> : null }
+         
+        </section>
+        
+      </main>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/components/TabButton.jsx
@@ -0,0 +1,5 @@
+export default function TabButton({children, onClick, isSelected}){
+    
+    return <li><button className={isSelected ? 'active' : undefined} onClick={onClick}>{children}
+    </button></li>;
+};
